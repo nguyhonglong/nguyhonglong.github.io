061e687624ea20ccf1cee48735877414ebd0da86
daniel_nguy  魏洪龙 <daniel_nguy@accton.com.vn>
2024-09-20 16:12:35 +0700

    push

---
+++ _astro/url-utils.4zWqSB7W.js
@@ -0,0 +1,1 @@
+import"./zh_TW.DZ_ffLrl.js";function i(e,r){return e.replace(/^\/|\/$/g,"").toLowerCase()===r.replace(/^\/|\/$/g,"").toLowerCase()}function t(...e){return e.join("/").replace(/\/+/g,"/")}function u(e){return t("","/nguyhonglong.github.io/",e)}export{i as p,u};
